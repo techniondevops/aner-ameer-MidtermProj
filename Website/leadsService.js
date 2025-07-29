77cfdee25da53b3a987eff64717026e435025c39
anern13 <anern13@gmail.com>
2025-07-29 14:47:40 +0300

    task 8 port change

--- Website/leadsService.js
+++ Website/leadsService.js
@@ -1,6 +1,6 @@
 angular.module('crudApp')
     .service('LeadsService', ['$http', function($http) {
-        const APIrootURL = "http://127.0.0.1:5000/leads";
+        const APIrootURL = "http://127.0.0.1:8080/leads";
         return {
             getLeads: getLeads,
             updateLead: updateLead,
